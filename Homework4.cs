4710948efe28b5c7ef64e32ebf1a8cb536e8c1c8
Gordon-Thetford <156831228+Gordon-Thetford@users.noreply.github.com>
2024-02-12 22:34:34 -0600

    Homework 4 Code File

---
+++ Homework4.cs
@@ -0,0 +1,67 @@
+﻿namespace Homework4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        // Question 1
+        Console.WriteLine("Input 2 numbers");
+        int x = Convert.ToInt16(Console.ReadLine());
+        int y = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Largest number is " + LargestNumber(x, y));
+
+        // Question 2
+        Console.WriteLine("Input a number");
+        int z = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("...and a shape- either 'left' or 'right'");
+        string shape = Console.ReadLine();
+        Console.WriteLine($"N is: {z}; shape is {shape}");
+        Triangle(z, shape);
+
+    }
+
+
+    static int LargestNumber(int x, int y)
+    {
+        if(x > y)
+        {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    static void Triangle(int z, string shape)
+    {
+        if(shape == "left") 
+        {
+            for(int row = 0; row < z; row ++)
+            {
+                for(int col = 0; col <= row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        } else if(shape == "right")
+        {
+            for(int row = 0; row < z; row ++)
+            {
+                for(int space = z-1-row; space > 0; space--)
+                {
+                    Console.Write(" ");
+                }
+                for(int ast = -1; ast < row; ast++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        } else {
+            Console.WriteLine("Error! Please try again.");
+        }
+    }
+}
+
+
